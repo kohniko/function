edcd127b8976a1ab7aad495f2d9020ab078b2845
kohniko <mcoulon@edenschool.fr>
2023-11-06 15:39:42 +0100

    0.0.1

---
+++ main.js
@@ -0,0 +1,19 @@
+let paragraph = document.createElement('p');
+paragraph.textContent = 'hello';
+document.querySelector('#app').appendChild(paragraph);
+
+let paragraph2 = document.createElement('p');
+paragraph2.textContent = 'bonjour';
+document.querySelector('#app').appendChild(paragraph2);
+
+let paragraph3 = document.createElement('p');
+paragraph3.textContent = 'coucou';
+document.querySelector('#app').appendChild(paragraph3);
+
+let paragraph4 = document.createElement('p');
+paragraph4.textContent = 'salut';
+document.querySelector('#app').appendChild(paragraph4);
+
+let paragraph5 = document.createElement('p');
+paragraph5.textContent = 'ola';
+document.querySelector('#app').appendChild(paragraph5);
